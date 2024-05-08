6510446edddbd87ad671afc573593cd5080ff8c8
russiantech <chrisjsmez@gmail.com>
2024-05-08 05:01:48 +0000

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
+"""Pascal Triangle Interview Challenge"""
 
 
 def pascal_triangle(n):
-    """Mak Pascal's triangle up to nth row."""
+    """returns a list of lists of nums
+    repr the pascal triangle"""
     if n <= 0:
         return []
 
-    triangle = [[1]]
+    triangle = []
 
-    for i in range(1, n):
-        row = [1]
+    for i in range(n):
+        triangle.append([])
+        triangle[i].append(1)
+
         for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)
-        triangle.append(row)
+            x = triangle[i-1][j-1]
+            y = triangle[i-1][j]
+            triangle[i].append(x+y)
+
+        if(n != 0 and i != 0):
+            triangle[i].append(1)
 
     return triangle
