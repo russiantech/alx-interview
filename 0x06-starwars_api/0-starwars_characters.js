7472c304e4e0cf0232d05815f6e5be916e7c0a3e
russiantech <chrisjsme@gmail.com>
2024-06-07 08:30:29 +0000

    0. Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,58 +2,26 @@
 
 const request = require('request');
 
-/**
- * Makes a request to a given URL and prints the character's name.
- * @param {string[]} urls - The list of character URLs.
- * @param {number} index - The current index in the list of URLs.
- */
-const fetchCharacter = (urls, index) => {
-  if (index === urls.length) return;
-
-  request(urls[index], (error, response, body) => {
-    if (error) {
-      console.error(`Error fetching character: ${error.message}`);
-      return;
+const requ = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      requ(arr, i + 1);
     }
-
-    try {
-      const character = JSON.parse(body);
-      console.log(character.name);
-    } catch (parseError) {
-      console.error(`Error parsing response: ${parseError.message}`);
-    }
-
-    fetchCharacter(urls, index + 1);
   });
 };
 
-/**
- * Fetches and prints all characters of a Star Wars movie by its ID.
- */
-const fetchMovieCharacters = () => {
-  const movieId = process.argv[2];
-
-  if (!movieId) {
-    console.error('Please provide a Movie ID');
-    return;
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      requ(chars, 0);
+    }
   }
-
-  const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-
-  request(url, (error, response, body) => {
-    if (error) {
-      console.error(`Error fetching movie: ${error.message}`);
-      return;
-    }
-
-    try {
-      const movie = JSON.parse(body);
-      const characters = movie.characters;
-      fetchCharacter(characters, 0);
-    } catch (parseError) {
-      console.error(`Error parsing response: ${parseError.message}`);
-    }
-  });
-};
-
-fetchMovieCharacters();
+);
